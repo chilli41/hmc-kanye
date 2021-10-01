c2a6de5ec29795f7b73ce8f243645774027b3941
chilli41 <cs.ibabao@gmail.com>
2021-10-01 05:49:13 +0000

    System can already fetch and display 5 random quotes from the api provided. Login authentication is already working.

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
@@ -25,14 +24,54 @@ class HomeController extends Controller
     public function index()
     {
         if(Auth::guest() == false){
-dd('hre');
-            $client = new Client();
-            $request = $client->get('https://api.kanye.rest');
-            $response = $request->getBody();
-            dd($response);
-//            return view('welcome');
+            try{
+                $kanye_result = $this->multiply_kanye(5);
+                return view('welcome')->with(compact('kanye_result'));
+            }catch(RequestException $e){
+                dd($e->getMessage());
+            }
+
         }else{
             return view('auth.login');
 	}
     }
+    
+/***********
+* Kanye API Mulitplier
+***********/
+    private function multiply_kanye($kanye_qoute_multiplier){
+        $kanye_multiplier = 0;
+        $kanye_result_arr = array();
+        try{
+            $kanye_multiplier += $kanye_qoute_multiplier;
+            for($kanye_ctr = 0; $kanye_ctr < $kanye_multiplier; $kanye_ctr++){
+                 array_push($kanye_result_arr, $this->get_kanye());
+            }
+        }catch(RequestException $e){
+            dd($e->getMessage());
+        }
+        return($kanye_result_arr);
+    }    
+
+/***********
+* Function to fetch kanye api
+***********/
+
+    private function get_kanye(){
+        $result_arr_kanye= array();
+        try{
+            $client = new \GuzzleHttp\Client();
+
+            $request = $client->get('https://api.kanye.rest');
+
+            $response = $request->getBody();
+
+            $result_arr_kanye = json_decode($response,true);
+        }catch(RequestException $e){
+            dd($e->getMessage());
+        }
+        return $result_arr_kanye['quote'];
+    }
+
+
 }
